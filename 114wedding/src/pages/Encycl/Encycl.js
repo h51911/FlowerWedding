d30d2d7188180586f43472471b25ac08920567c8
codeboy0417 <56332641+codeboy0417@users.noreply.github.com>
2020-01-08 08:59:52 +0800

    <修改bug>

--- 114wedding/src/pages/Encycl/Encycl.js
+++ 114wedding/src/pages/Encycl/Encycl.js
@@ -1,13 +1,60 @@
 import React, { Component } from 'react';
+import { Carousel, Row, Col, Tabs } from 'antd';
 
+import '../../css/encycl/encycl.css'
 class Encycl extends Component {
     state = {
-
+        datalist: [
+            {
+                url: 'https://www.wed114.cn/wiki/static/images/yibu.jpg',
+                title: '跨年狂欢！拍婚纱照送全家福！！！'
+            },
+            {
+                url: 'https://www.wed114.cn/wiki/static/images/erbu.jpg',
+                title: '广州爱城婚纱摄影天河店'
+            },
+            {
+                url: 'https://www.wed114.cn/wiki/static/images/sanbu.jpg',
+                title: '广州田野婚纱摄影工作室'
+            },
+            {
+                url: 'https://www.wed114.cn/wiki/static/images/sibu.jpg',
+                title: '广州田野婚纱摄影工作室'
+            },
+            {
+                url: 'https://www.wed114.cn/wiki/static/images/wubu.jpg',
+                title: '广州田野婚纱摄影工作室'
+            },
+            {
+                url: 'https://www.wed114.cn/wiki/static/images/liubu.jpg',
+                title: '广州田野婚纱摄影工作室'
+            }
+        ]
     }
     render() {
+        let { datalist } = this.state;
         return <>
-            <header></header>
-            <main></main>
+            <header className="header">
+                <Carousel autoplay>
+                    {datalist.map((item, index) => (
+                        <div>
+                            <img src={item.url} alt={item.title} />
+                        </div>
+                    ))}
+                </Carousel>,
+        </header>
+            <main className="main">
+                {/* <section className="nav">
+                    <div className="title">婚嫁百科</div>
+                    <ul>{list.map((item, index) => (
+                        <li>
+                            <img src={item.url} />
+                            <p>{item.text}</p>
+                        </li>
+                    ))}
+                    </ul>
+                </section> */}
+            </main>
         </>
     }
 
--- 114wedding/src/pages/Home/Home.js
+++ 114wedding/src/pages/Home/Home.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Carousel, Row, Col, Tabs } from 'antd';
 
-
 import '../../css/home/home.css'
 import Tab from './Tabs'
 import img1 from '../../doc/home/left.png'
@@ -11,130 +10,136 @@ const { TabPane } = Tabs;
 
 
 class Home extends Component {
-    state = {
-        datalist: [
-            {
-                url: 'https://pic18.wed114.cn/20191216/2019121614162692331407.jpg',
-                title: '跨年狂欢！拍婚纱照送全家福！！！'
-            },
-            {
-                url: 'https://pic18.wed114.cn/20200102/2020010215000639105468.jpg',
-                title: '广州爱城婚纱摄影天河店'
-            },
-            {
-                url: 'https://pic18.wed114.cn/20191223/2019122317584872149360.jpg',
-                title: '广州田野婚纱摄影工作室'
-            },
-        ],
-        list: [
-            {
-                url: 'https://static3.wed114.cn/mobile/v2/img/@2x/Photography@2x.png',
-                text: '婚纱摄影'
-            },
-            {
-                url: 'https://static3.wed114.cn/mobile/v2/img/@2x/Plan@2x.png',
-                text: '婚礼策划'
-            },
-            {
-                url: 'https://static3.wed114.cn/mobile/v2/img/@2x/dress@2x.png',
-                text: '婚纱礼服'
-            },
-            {
-                url: 'https://static3.wed114.cn/mobile/v2/img/@2x/icon_xngz.png',
-                text: '新娘跟妆'
-            },
-            {
-                url: 'https://static3.wed114.cn/mobile/v2/img/@2x/icon_hlgp.png',
-                text: '婚礼跟拍'
-            },
-            {
-                url: 'https://static3.wed114.cn/mobile/v2/img/@2x/icon_siyi.png',
-                text: '婚礼司仪'
-            },
-            {
-                url: 'https://static3.wed114.cn/mobile/v2/img/@2x/icon_dhy.png',
-                text: '婚宴酒店'
-            },
-            {
-                url: 'https://static3.wed114.cn/mobile/v2/img/@2x/icon_photokids.png',
-                text: '儿童摄影'
-            },
-
-        ],
-        listvideo: [
-            {
-                url: 'https://pic11.wed114.cn/pic10/20190807/2019080710053612861578x600_400_0.jpg',
-                title: '2019下半年新品发布',
-                text: '广州爱城婚纱摄影天河店',
-                num: '12260'
-            },
-            {
-                url: 'https://pic11.wed114.cn/pic10/20190807/2019080710044565378621x600_400_0.jpg',
-                title: '2019下半年新品发布',
-                text: '广州爱城婚纱摄影天河店',
-                num: '12043'
-            },
-            {
-                url: 'https://pic11.wed114.cn/pic10/20190807/2019080709590148024012x600_400_0.jpg',
-                title: '2019下半年新品发布',
-                text: '广州爱城婚纱摄影天河店',
-                num: '12546'
-            },
-            {
-                url: 'https://pic11.wed114.cn/pic10/20190807/2019080709540927716025x600_400_0.jpg',
-                title: '2019下半年新品发布',
-                text: '广州爱城婚纱摄影天河店',
-                num: '12486'
-            },
-            {
-                url: 'https://pic11.wed114.cn/pic10/20190521/2019052116255429468509x600_400_0.jpg',
-                title: 'Awei高级西服定制',
-                text: 'Awei西服定制',
-                num: '17592'
-            },
-            {
-                url: 'https://pic11.wed114.cn/pic10/20181126/2018112611153704028117x600_400_0.jpg',
-                title: '爱城摄影 热推场景',
-                text: '广州爱城婚纱摄影天河店',
-                num: '8168'
-            },
-        ],
-        menu: [
-            {
-                title: '全部'
-            },
-            {
-                title: '婚纱摄影'
-            },
-            {
-                title: '婚礼策划'
-            },
-            {
-                title: '婚纱礼服'
-            },
-            {
-                title: '新娘跟妆'
-            },
-            {
-                title: '婚礼跟拍'
-            },
-            {
-                title: '婚礼司仪'
-            },
-            {
-                title: '儿童摄影'
-            },
-        ]
+    constructor() {
+        super();
 
 
-        //this.callback = this.callback.bind(this);
+        this.state = {
+            datalist: [
+                {
+                    url: 'https://pic18.wed114.cn/20191216/2019121614162692331407.jpg',
+                    title: '跨年狂欢！拍婚纱照送全家福！！！'
+                },
+                {
+                    url: 'https://pic18.wed114.cn/20200102/2020010215000639105468.jpg',
+                    title: '广州爱城婚纱摄影天河店'
+                },
+                {
+                    url: 'https://pic18.wed114.cn/20191223/2019122317584872149360.jpg',
+                    title: '广州田野婚纱摄影工作室'
+                },
+            ],
+            list: [
+                {
+                    url: 'https://static3.wed114.cn/mobile/v2/img/@2x/Photography@2x.png',
+                    text: '婚纱摄影'
+                },
+                {
+                    url: 'https://static3.wed114.cn/mobile/v2/img/@2x/Plan@2x.png',
+                    text: '婚礼策划'
+                },
+                {
+                    url: 'https://static3.wed114.cn/mobile/v2/img/@2x/dress@2x.png',
+                    text: '婚纱礼服'
+                },
+                {
+                    url: 'https://static3.wed114.cn/mobile/v2/img/@2x/icon_xngz.png',
+                    text: '新娘跟妆'
+                },
+                {
+                    url: 'https://static3.wed114.cn/mobile/v2/img/@2x/icon_hlgp.png',
+                    text: '婚礼跟拍'
+                },
+                {
+                    url: 'https://static3.wed114.cn/mobile/v2/img/@2x/icon_siyi.png',
+                    text: '婚礼司仪'
+                },
+                {
+                    url: 'https://static3.wed114.cn/mobile/v2/img/@2x/icon_dhy.png',
+                    text: '婚宴酒店'
+                },
+                {
+                    url: 'https://static3.wed114.cn/mobile/v2/img/@2x/icon_photokids.png',
+                    text: '儿童摄影'
+                },
+
+            ],
+            listvideo: [
+                {
+                    url: 'https://pic11.wed114.cn/pic10/20190807/2019080710053612861578x600_400_0.jpg',
+                    title: '2019下半年新品发布',
+                    text: '广州爱城婚纱摄影天河店',
+                    num: '12260'
+                },
+                {
+                    url: 'https://pic11.wed114.cn/pic10/20190807/2019080710044565378621x600_400_0.jpg',
+                    title: '2019下半年新品发布',
+                    text: '广州爱城婚纱摄影天河店',
+                    num: '12043'
+                },
+                {
+                    url: 'https://pic11.wed114.cn/pic10/20190807/2019080709590148024012x600_400_0.jpg',
+                    title: '2019下半年新品发布',
+                    text: '广州爱城婚纱摄影天河店',
+                    num: '12546'
+                },
+                {
+                    url: 'https://pic11.wed114.cn/pic10/20190807/2019080709540927716025x600_400_0.jpg',
+                    title: '2019下半年新品发布',
+                    text: '广州爱城婚纱摄影天河店',
+                    num: '12486'
+                },
+                {
+                    url: 'https://pic11.wed114.cn/pic10/20190521/2019052116255429468509x600_400_0.jpg',
+                    title: 'Awei高级西服定制',
+                    text: 'Awei西服定制',
+                    num: '17592'
+                },
+                {
+                    url: 'https://pic11.wed114.cn/pic10/20181126/2018112611153704028117x600_400_0.jpg',
+                    title: '爱城摄影 热推场景',
+                    text: '广州爱城婚纱摄影天河店',
+                    num: '8168'
+                },
+            ],
+            menu: [
+                {
+                    title: '全部'
+                },
+                {
+                    title: '婚纱摄影'
+                },
+                {
+                    title: '婚礼策划'
+                },
+                {
+                    title: '婚纱礼服'
+                },
+                {
+                    title: '新娘跟妆'
+                },
+                {
+                    title: '婚礼跟拍'
+                },
+                {
+                    title: '婚礼司仪'
+                },
+                {
+                    title: '儿童摄影'
+                },
+            ]
+
+
+            //this.callback = this.callback.bind(this);
+        }
+        this.change = this.change.bind(this)
     }
 
     callback(key) {
         console.log(key);
     }
     change() {
-
+        this.props.history.push('/list')
     }
     render() {
         let { datalist } = this.state;
@@ -142,19 +147,19 @@ class Home extends Component {
         let { listvideo } = this.state;
         let { menu } = this.state;
         return <>
-            <header className="header">
+            <div className="header">
                 <Carousel autoplay>
                     {datalist.map((item, index) => (
                         <div>
                             <img src={item.url} alt={item.title} />
                         </div>
                     ))}
-                </Carousel>,
-        </header>
+                </Carousel>
+            </div>
             <main className="main">
                 <section className="nav">
                     <ul>{list.map((item, index) => (
-                        <li>
+                        <li onClick={this.change}>
                             <img src={item.url} />
                             <p>{item.text}</p>
                         </li>
