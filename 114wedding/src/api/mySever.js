0fa84e680ac78a3435ece4d68aecda6dd79d10a7
qiansuzhen <qsz@qsz#qsz$qsz%123>
2020-01-10 17:26:54 +0800

    Update mySever.js

--- 114wedding/src/api/mySever.js
+++ 114wedding/src/api/mySever.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+import qs from 'qs'
 
 const Sever = axios.create({
-  baseURL: "http://10.3.140.72:1912/"
+  baseURL: "http://localhost:1912/"
 });
 
 export const get = async (url, params, config = {}) => {
@@ -12,7 +13,7 @@ export const get = async (url, params, config = {}) => {
   return data;
 };
 export const post = async (path, data = {}, config = {}) => {
-  return await Sever.post(path, data, config);
+  return await Sever.post(path, qs.stringify(data), config);
 };
 
 export default {
